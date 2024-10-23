0742556331d7e95d51e8c2740fa11f774c7b8754
Julien Castets <castets.j@gmail.com>
2024-10-23 10:23:56 +0200

    Setup prettier for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <h1 className="bg-green-200 text-center p-10">
-      Plakar UI
-    </h1>
-  )
+  return <h1 className="bg-green-200 p-10 text-center">Plakar UI</h1>;
 }
 
-export default App
+export default App;
